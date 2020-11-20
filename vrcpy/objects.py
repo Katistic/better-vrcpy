c4095dc867edda3dc39cc2e813c438b0bcd241b9
Katistix <33645731+Katistic@users.noreply.github.com>
2020-11-20 19:00:55 +1100

    CurrentUser

--- vrcpy/objects.py
+++ vrcpy/objects.py
@@ -172,3 +172,114 @@ class User(LimitedUser):
         if obj is not None:
             self._assign(obj)
 
+class CurrentUser(User):
+    def __init__(self, client, obj, loop=None):
+        super().__init__(client, loop=loop)
+
+        self.required.update({
+            "past_display_names": {
+                "dict_key": "pastDisplayNames",
+                "type": list
+            },
+            "email_verified": {
+                "dict_key": "emailVerified",
+                "type": bool
+            },
+            "has_email": {
+                "dict_key": "hasEmail",
+                "type": bool
+            },
+            "has_pending_email": {
+                "dict_key": "hasPendingEmail",
+                "type": bool
+            },
+            "accepted_tos_version": {
+                "dict_key": "acceptedTOSVersion",
+                "type": int
+            },
+            "has_birthday": {
+                "dict_key": "hasBirthday",
+                "type": bool
+            },
+            "friends": {
+                "dict_key": "friends",
+                "type": list
+            },
+            "online_friends": {
+                "dict_key": "onlineFriends",
+                "type": list
+            },
+            "active_friends": {
+                "dict_key": "activeFriends",
+                "type": list
+            },
+            "offline_friends": {
+                "dict_key": "offlineFriends",
+                "type": str
+            },
+            "friend_group_names": {
+                "dict_key": "friendGroupNames",
+                "type": list
+            },
+            "avatar": {
+                "dict_key": "currentAvatar",
+                "type": dict
+            },
+            "avatar_asset_url": {
+                "dict_key": "currentAvatarAssetUrl",
+                "type": str
+            },
+            "home_location": {
+                "dict_key": "homeLocation",
+                "type": str
+            },
+            "has_logged_in_from_client": {
+                "dict_key": "hasLoggedInFromClient",
+                "type": bool
+            },
+            "mfa_enabled": {
+                "dict_key": "twoFactorAuthEnabled",
+                "type": bool
+            },
+            "unsubscribe": {
+                "dict_key": "unsubscribe",
+                "type": bool
+            },
+            "feature": {
+                "dict_key": "feature",
+                "type": dict
+            }
+        })
+
+        self.optional.update({
+            "email": {
+                "dict_key": "email",
+                "type": str
+            },
+            "obfuscated_email": {
+                "dict_key": "obfuscatedEmail",
+                "type": str
+            },
+            "obfuscated_pending_email": {
+                "dict_key": "obfuscatedPendingEmail",
+                "type": str
+            },
+            "steam_id": {
+                "dict_key": "steamId",
+                "type": str
+            },
+            "steam_details": {
+                "dict_key": "steamDetails",
+                "type": dict
+            },
+            "oculus_id": {
+                "dict_key": "oculusId",
+                "type": str
+            },
+            "account_deletion_date": {
+                "dict_key": "accountDeletionDate",
+                "type": str
+            }
+        })
+
+        self._assign(obj)
